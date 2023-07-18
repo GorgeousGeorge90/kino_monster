a807fd04d4af1534dcf405b65248a0eefb814ab1
Egor <Gambit47@yandex.ru>
2023-07-18 16:41:08 +0300

    upd footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,24 +1,23 @@
 import {ReactComponent as Vk} from './img/vk1.svg';
 import {ReactComponent as Gh} from './img/gh.svg';
 import {ReactComponent as Tg} from './img/tg.svg';
-import styles from './Footer.module.scss';
 
 
 const Footer = () => {
 
-    return (<div className={styles.footer_container}>
-        <footer className={styles.footer}>
+    return (<div className={'flex items-center justify-center bg-secondary_light dark:bg-secondary_dark h-[70px]'}>
+        <footer className={'grid grid-cols-[repeat(3,50px)_250px] gap-2 items-center justify-center font-sans-serif'}>
         <a  className={'self-center place-self-center'}
             href='https://vk.com/gorgeous_george90'>
-            <Vk width={'2em'} className={styles.footer_icon}/>
+            <Vk width={'2em'} className={'cursor-pointer hover:fill-amber-200 fill-neutral-100'}/>
         </a>
         <a  className={'place-self-center'}
             href='https://t.me/MrStandart90'>
-            <Tg width={'2.8em'} className={styles.footer_icon}/>
+            <Tg width={'2.8em'} className={'cursor-pointer hover:fill-amber-200 fill-neutral-100'}/>
         </a>
         <a className={'place-self-center'}
             href='https://github.com/GorgeousGeorge90'>
-            <Gh className={styles.footer_icon_it}/>
+            <Gh className={'cursor-pointer hover:fill-amber-200 fill-neutral-100 scale-125'}/>
         </a>
         <p className={'text-neutral-100 place-self-center'}><i>check my GH for other works</i></p>
     </footer>
